d9cc9017720fa6b8c04155642567ba0c1576f99a
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2020-12-28 22:25:12 +0800

    v1.1.0

--- src/locale/version/index.js
+++ src/locale/version/index.js
@@ -5,6 +5,10 @@ export default {
     },
     "1_0_1": {
       "0": "Alert plaintext directly."
+    },
+    "1_1_0": {
+      "0": "Double click to decrypt!",
+      "1": "Decrypt automatically!"
     }
   },
   zhCn: {
@@ -13,6 +17,10 @@ export default {
     },
     "1_0_1": {
       "0": "解密后自动弹窗显示明文。"
+    },
+    "1_1_0": {
+      "0": "双击密文快捷解密",
+      "1": "密文自动解密"
     }
   }
 }

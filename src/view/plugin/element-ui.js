8a4eb182df51e0cf12bb297a64985e4ddacb85ed
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-03-01 10:43:23 +0800

    Reomve semicolons

--- src/view/plugin/element-ui.js
+++ src/view/plugin/element-ui.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI);

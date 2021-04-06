62c3bd03cfa0318221bfeba76c14d41d18d85d9e
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-04-06 14:54:59 +0800

    open guide in firefox

--- src/zero/common/guide-opener.ts
+++ src/zero/common/guide-opener.ts
@@ -5,15 +5,16 @@ import Initializable from "../../chrome/interface/initializable"
  *  
  * @since 1.4.0
  */
-export function openGuide() {
-  const isFireFox = /Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)
+export function openGuide(includingStatic?: boolean) {
+  const ua = navigator.userAgent
+  includingStatic = includingStatic || !/Firefox[\/\s](\d+\.\d+)/.test(ua)
   // FireFox use 'static' as prefix
-  const url = isFireFox ? 'guide.html' : 'static/guide.html'
+  const url = includingStatic ? 'static/guide.html' : 'guide.html'
   chrome.tabs.create({ url })
 }
 
 export default class GuideOpener implements Initializable {
   initialize(): void {
-    openGuide()
+    openGuide(true)
   }
 }

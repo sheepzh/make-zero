5820c077a7a7962c7ef778772f088868f76f240f
returnzhy1996@outlook.com <returnzhy1996@outlook.com>
2021-05-27 19:17:09 +0800

    Use composition APIs

--- src/view/guide/components/basic/head.ts
+++ src/view/guide/components/basic/head.ts
@@ -1,36 +1,34 @@
-import { defineComponent, h, reactive } from "vue"
+import { defineComponent, h, ref } from "vue"
 import { ElAlert } from "element-plus"
 import cryptorConfig from "../../../../config"
 import { t } from "../../../plugin/i18n"
+import { boldAndItalic } from "../../../util/vue-util"
 
 /**
  * Head of this page
  */
-export default defineComponent({
-  name: 'BasicHead',
-  setup(_props) {
-    return reactive({ password: '' })
-  },
-  created() {
-    cryptorConfig.getPassword().then(p => this.password = p)
-  },
-  render(_ctx: any) {
-    return h('div', {},
-      [
-        // title
-        h('h2', {}, t('guide.basic.title')),
-        // alert
-        h(ElAlert,
-          { type: 'success' },
-          {
-            title: () => [
-              t('guide.basic.currentPassword'),
-              h('b', {}, h('i', {}, _ctx.password)),
-              t('guide.period')
-            ]
-          }
-        )
-      ]
-    )
-  }
+const _default = defineComponent<{}, {}>(() => {
+  const password = ref('')
+
+  cryptorConfig.getPassword().then(p => password.value = p)
+
+  return () => h('div',
+    [
+      // title
+      h('h2', t('guide.basic.title')),
+      // alert
+      h(ElAlert,
+        { type: 'success' },
+        {
+          title: () => [
+            t('guide.basic.currentPassword'),
+            boldAndItalic(password.value),
+            t('guide.period')
+          ]
+        }
+      )
+    ]
+  )
 })
+
+export default _default
